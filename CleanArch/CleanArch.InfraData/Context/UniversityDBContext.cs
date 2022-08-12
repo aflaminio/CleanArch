92e2ea489c39e90f9cc5f430544f5ba937cd531e
Ariel Flaminio <arielflaminio@gmail.com>
2022-08-12 00:40:44 -0300

    Update data Layer and also update presentation layer for University context

--- CleanArch/CleanArch.InfraData/Context/UniversityDBContext.cs
+++ CleanArch/CleanArch.InfraData/Context/UniversityDBContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CleanArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace CleanArch.InfraData.Context
 {
-    public class UniversityDBContext
-    {
+    public class UniversityDBContext : DbContext
+    { 
+        public UniversityDBContext (DbContextOptions options) :base(options)
+        { }
+
+        public DbSet<Course> Courses { get; set; }
     }
 }
--- CleanArch/CleanArch.Mvc/Program.cs
+++ CleanArch/CleanArch.Mvc/Program.cs
@@ -1,6 +1,7 @@
 using CleanArch.Mvc.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using CleanArch.InfraData.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,11 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<UniversityDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UniversityDBConnection"));
+});
 
 var app = builder.Build();
 
