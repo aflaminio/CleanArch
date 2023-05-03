49a852735a1b20a9c976bb51a19c871149511cd7
Ariel Flaminio <arielflaminio@gmail.com>
2023-05-03 20:21:01 -0300

    Updated Course Service

--- CleanArch/CleanArch.Application/Services/CourseService.cs
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -1,6 +1,8 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
 using CleanArch.Domain.Interfaces;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +13,21 @@ namespace CleanArch.Application.Services
 {
     public class CourseService : ICourseService
     {
-        private ICourseRepository _courseRepository;
+        private readonly ICourseRepository _courseRepository;
+        private readonly IMediator _bus;
 
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediator bus)
         {
-            _courseRepository = courseRepository;        
+            _courseRepository = courseRepository;  
+            _bus = bus;
         }
+
+        public void Create(CourseViewModel course)
+        {
+            var createCourseCommand = new CreateCourseCommand(course.Name, course.Description, course.ImageUrl);
+            _bus.Send(createCourseCommand);
+        }
+
         public CourseViewModel GetCourses()
         {
             return new CourseViewModel()
