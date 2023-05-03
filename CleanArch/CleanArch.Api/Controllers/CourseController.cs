555f3bdfb5576d702a036e1997b2d6f5eded4634
Ariel Flaminio <arielflaminio@gmail.com>
2023-05-03 20:33:42 -0300

    Added API Project

---
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+
+    }
+}
+
+       
